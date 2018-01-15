ec229154846454de0ac96a4d441eccdb2b0ed400
TatyCat <williams.tatyanna@gmail.com>
2018-01-15 01:02:16 -0500

    Fixed Routing, Pending Form Save Logic

--- todolist/urls.py
+++ todolist/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     url(r'^$', views.todo, name='todo'),
+    url(r'^new_todo/$', views.new_todo, name='new_todo'),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,4 +1,31 @@
 from django.shortcuts import render
+from .models import Todo
+from .forms import TodoForm
+from django.utils import timezone
+from django.shortcuts import redirect
+# from django.http import HttpResponse
 
 def todo(request):
-    return render(request, 'todolist/todo_list.html')
+    todos = Todo.objects.all()
+    return render(request, 'todolist/todo_list.html', {'listitem': todos})
+
+def new_todo(request):
+    if request.method == "POST":
+        if form.is_valid():
+            text= request.POST['text']
+            # new_todo.created_date = timezone.now()
+            new_todo = Todo(text=text) 
+            new_todo.save()
+    else:
+        form = TodoForm()
+        return render(request, 'todolist/todo_list.html', {'form': form})
+
+'''
+if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            new_todo = form.save(commit=False)
+            new_todo.created_date = timezone.now()
+            new_todo.save()
+            return redirect('todo')
+'''
